da77e9e669c9814aeb69a228f5e95d6f1dc151f9
zy0228 <2472305432@qq.com>
2020-07-16 21:12:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Counter from './components/Counter.jsx'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `you clicked ${count} times`
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter></Counter>
       </header>
     </div>
   );
